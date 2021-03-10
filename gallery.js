baecbded4ffd931cb4a7eb1523133129490a6928
kaylee <70932714+kaygelz@users.noreply.github.com>
2021-03-09 22:59:16 -0800

    image node

--- gallery.js
+++ gallery.js
@@ -4,3 +4,21 @@
 ///MARCH 9
 ///ASSIGMENT 7
 
+class Image {
+    constructor (imgName, Description) {
+        this.imgName = imgName;
+        this.Description = Description;
+    }
+    let image1 = new Image(1);
+    let image2 = new Image(2);
+    let image3 = new Image(3);
+    let image4 = new Image(4);
+    let image5 = new Image(5);
+}
+
+var galleryPhotos = [image1, image2, image3, image4, image5];
+
+
+    
+}
+
